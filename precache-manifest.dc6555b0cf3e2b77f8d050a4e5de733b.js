7916529a18dd89ddcac28153d83c8aac28ee50f9
agamystar <orcl.masr@hotmail.com>
2020-05-03 21:54:14 +0200

    first commit

---
+++ precache-manifest.dc6555b0cf3e2b77f8d050a4e5de733b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba383d05d39d448a2691ef7e85507025",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2433151d101f619648b3",
+    "url": "/static/css/main.97164268.chunk.css"
+  },
+  {
+    "revision": "740658ce7cfb9021bef6",
+    "url": "/static/js/2.65ec1916.chunk.js"
+  },
+  {
+    "revision": "6b3248a1692933689dcb27b9e1ed5092",
+    "url": "/static/js/2.65ec1916.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2433151d101f619648b3",
+    "url": "/static/js/main.f30a2247.chunk.js"
+  },
+  {
+    "revision": "c71849e6b92a54d9b65d",
+    "url": "/static/js/runtime-main.dbac8e30.js"
+  }
+]);
